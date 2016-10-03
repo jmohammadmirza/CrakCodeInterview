7318f33f6c037f16e9308c0c52f0601a61b1918d
Mohammad Javed <mdaslameme@gmail.com>
2016-10-03 18:54:08 +0530

    Create Ransom.java

---
+++ Ransom.java
@@ -0,0 +1,88 @@
+A kidnapper wrote a ransom note but is worried it will be traced back to him. He found a magazine and wants to know if he can cut out whole words from it and use them to create an untraceable replica of his ransom note. The words in his note are case-sensitive and he must use whole words available in the magazine, meaning he cannot use substrings or concatenation to create the words he needs.
+
+Given the words in the magazine and the words in the ransom note, print Yes if he can replicate his ransom note exactly using whole words from the magazine; otherwise, print No.
+
+Input Format
+
+The first line contains two space-separated integers describing the respective values of  (the number of words in the magazine) and  (the number of words in the ransom note). 
+The second line contains  space-separated strings denoting the words present in the magazine. 
+The third line contains  space-separated strings denoting the words present in the ransom note.
+
+Constraints
+
+.
+Each word consists of English alphabetic letters (i.e.,  to  and  to ).
+The words in the note and magazine are case-sensitive.
+Output Format
+
+Print Yes if he can use the magazine to create an untraceable replica of his ransom note; otherwise, print No.
+
+Sample Input
+
+6 4
+give me one grand today night
+give one grand today
+Sample Output
+
+Yes
+Explanation
+
+All four words needed to write an untraceable replica of the ransom note are present in the magazine, so we print Yes as our answer.
+
+**************************************************************************************************************************
+
+
+
+
+import java.util.*;
+
+public class Solution {
+    private String magazine;
+    private String note;
+      List<String> l=new ArrayList<String>();
+      List<String> l2=new ArrayList<String>();
+    private int check=0;
+   
+    public Solution(String magazine, String note) {
+     this.magazine=magazine;
+     this.note=note;
+    }
+    
+    public boolean solve() {
+      boolean flag=false;
+      String temp[]=magazine.split(" ");
+	  for(String s:temp){
+	         l.add(s);
+	        	}
+     String temp1[]=note.split(" ");
+	  for(String s:temp1){
+	        l2.add(s);
+	   }
+      Iterator itr=l2.iterator();
+        while(itr.hasNext()){
+            if(l.contains(itr.next())) 
+              check++;
+        }
+        if(check==l2.size())
+               flag=true;
+        return flag;      
+    }
+   
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int m = scanner.nextInt();
+        int n = scanner.nextInt();
+        
+        // Eat whitespace to beginning of next line
+        scanner.nextLine();
+        
+        Solution s = new Solution(scanner.nextLine(), scanner.nextLine());
+        scanner.close();
+       
+        boolean answer = s.solve();
+        if(answer)
+            System.out.println("Yes");
+        else System.out.println("No");
+      
+    }
+}
