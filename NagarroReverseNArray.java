1fa15ebecd5b5d9ae3f6aaa194fef642a728ea2a
Mohammad Javed <mdaslameme@gmail.com>
2018-01-08 16:37:17 +0530

    Update NagarroReverseNArray.java
    prettify...

--- NagarroReverseNArray.java
+++ NagarroReverseNArray.java
@@ -7,41 +7,41 @@ import java.util.List;
 
 /*
 Reverse First N elements in the array Solution
-*/
+ */
 public class Nagarro {
-	
+
 	public static void main(String[] args) {
-	int temp=0;
-	int n=4;
+		int temp=0;
+		int n=4;
 		int[] a={2,3,4,5,6,7,8,9};
-		
-  /*	for(int i=0;i<n--;i++){
+
+		/*	for(int i=0;i<n--;i++){
 			temp=a[i];
 			a[i]=a[n];
 			a[n]=temp;
-      //pure array based no Lists used 
-			
+                //pure array based no Lists used 
+
 		}
-		*/
+		 */
 		// Using List little complicated
-    
+
 		List<Integer> al=new ArrayList<Integer>();
-		
+
 		for(int i:a){
 			al.add(i);
 		}
-		
+
 		Collections.reverse(al);
 		//Print List
 		System.out.println("List Print "+al);
-		
+
 		int[] array = al.stream().mapToInt(i->i).toArray();
-		
+
 		//Print Array
-    System.out.println("Array Print"+Arrays.toString(array));
-    //Print Array without Square brackets
-    System.out.println("Array Print sans Brackets "+Arrays.toString(array).replace("[","").replace("]",""));
-    
+		System.out.println("Array Print"+Arrays.toString(array));
+		//Print Array without Square brackets
+		System.out.println("Array Print sans Brackets "+Arrays.toString(array).replace("[","").replace("]",""));
+
 	}
 
 }
